a6fec21c7b5102c3e7f2658b43dfc0b0e35f22ed
sarobidy <hei.sarobidy.6pgmail.com>
2025-08-07 00:22:27 +0300

    modification

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,14 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Body from './composant/Body';
+import Poke from './composant/Poke.jsx';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Body/>
+      <Poke/>
     </>
   )
 }
